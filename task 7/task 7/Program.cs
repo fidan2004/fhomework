1099f7db13f99eb88448382ac93aa5e2ba952ac6
fidan2004 <110298605+fidan2004@users.noreply.github.com>
2022-08-11 12:37:42 +0400

    Add files via upload

---
+++ task 7/task 7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace task_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            Console.WriteLine(myfunct(sum));
+        }
+
+        static int myfunct(int num)
+        {
+            int reminder;
+            Console.Write("Enter the Number : ");
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+            while (number > 0)
+            {
+                reminder = number % 10;
+                sum = sum + reminder;
+                number = number / 10;
+            }
+            return sum;
+
+        }
+    }
+}
